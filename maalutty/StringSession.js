0a0b07814c4f9e7794fd3696fa6568ae1bbfa1a8
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-10 20:52:47 +0530

    IAM JOCKERSIR

--- maalutty/StringSession.js
+++ maalutty/StringSession.js
@@ -18,7 +18,7 @@ class StringSession {
 
     deCrypt(string = undefined) {
 
-        if ('MAALUTTY_CODE' in process.env && string === undefined) {
+        if ('JOKER_CODE' in process.env && string === undefined) {
 
             string = process.env.STRING_SESSION;
 
@@ -46,7 +46,7 @@ class StringSession {
 
     createStringSession(dict) {
 
-        return 'Maalutty;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Jokersir;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
 
     }
 
