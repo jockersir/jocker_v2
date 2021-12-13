920ba6f357f7e4efaf4c3d7fef3c33ea5670c849
jockersir <95922321+jockersir@users.noreply.github.com>
2021-12-13 21:46:22 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -88,7 +88,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Miss Helena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'jocker_v2.png', mimetype: Mimetype.png});
         await load.delete();
     }));
     
